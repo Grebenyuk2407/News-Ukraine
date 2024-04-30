d34250de2b6601b9e28a2a9c48d87d88d573c5dc
Anton Grebenyuk <antongrebenyuk1231@gmail.com>
2024-04-30 19:27:32 +0300

    finish all xml

---
+++ app/src/main/java/dev/androidbroadcast/newsukraine/ui/fragments/ArticleFragment.kt
@@ -0,0 +1,20 @@
+package dev.androidbroadcast.newsukraine.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import dev.androidbroadcast.newsukraine.R
+
+class ArticleFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_article, container, false)
+    }
+
+}
