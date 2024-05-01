0875db74259755d700d9a737df4d8219c455696f
Anton Grebenyuk <antongrebenyuk1231@gmail.com>
2024-05-01 21:54:57 +0300

    create viewModel

---
+++ app/src/main/java/dev/androidbroadcast/newsukraine/ui/NewsViewModelProviderFactory.kt
@@ -0,0 +1,12 @@
+package dev.androidbroadcast.newsukraine.ui
+
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import dev.androidbroadcast.newsukraine.repository.NewsRepository
+
+class NewsViewModelProviderFactory(val app:Application, val newsRepository:NewsRepository):ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return NewsViewModel(app, newsRepository) as T
+    }
+}
