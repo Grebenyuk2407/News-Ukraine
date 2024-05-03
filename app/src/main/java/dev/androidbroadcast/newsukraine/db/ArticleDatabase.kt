75a91bc80af508addff8a80c368b2ea1574d2011
Anton Grebenyuk <antongrebenyuk1231@gmail.com>
2024-05-03 22:36:03 +0300

    fix room

--- app/src/main/java/dev/androidbroadcast/newsukraine/db/ArticleDatabase.kt
+++ app/src/main/java/dev/androidbroadcast/newsukraine/db/ArticleDatabase.kt
@@ -9,22 +9,22 @@ import dev.androidbroadcast.newsukraine.models.Article
 
 @Database(
     entities = [Article::class],
-    version = 1
+    version = 2 // Увеличиваем версию базы данных
 )
 @TypeConverters(Converters::class)
-abstract class ArticleDatabase:RoomDatabase() {
+abstract class ArticleDatabase : RoomDatabase() {
 
-    abstract fun getArticleDao():ArticleDAO
+    abstract fun getArticleDao(): ArticleDAO
 
-    companion object{
+    companion object {
         @Volatile
-        private var instance :ArticleDatabase?= null
+        private var instance: ArticleDatabase? = null
         private val LOCK = Any()
 
-        operator fun invoke(context: Context) = instance?: synchronized(LOCK){
-          instance?: createDatabase(context).also {
-              instance = it
-          }
+        operator fun invoke(context: Context) = instance ?: synchronized(LOCK) {
+            instance ?: createDatabase(context).also {
+                instance = it
+            }
         }
 
         private fun createDatabase(context: Context) =
@@ -32,8 +32,7 @@ abstract class ArticleDatabase:RoomDatabase() {
                 context.applicationContext,
                 ArticleDatabase::class.java,
                 "article_db.db"
-            ).build()
-
-
+            ).fallbackToDestructiveMigration() // Добавляем эту строчку, чтобы Room выполнил разрушительную миграцию при изменении схемы базы данных
+                .build()
     }
 }
